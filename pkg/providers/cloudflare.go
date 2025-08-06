be71e26b229e82add56fe64c0142c73d49cc53ba
bohdan-vulnebify <bohdan@vulnebify.com>
2025-08-06 22:42:19 +0000

    Update A record on Cloudflare DNS provider on Fixes #1

--- pkg/providers/cloudflare.go
+++ pkg/providers/cloudflare.go
@@ -1,56 +1,60 @@
 package providers
 
 import (
-	"os"
-	"fmt"
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/cloudflare/cloudflare-go/v5"
+	"github.com/cloudflare/cloudflare-go/v5/dns"
 	"github.com/cloudflare/cloudflare-go/v5/option"
 	"github.com/cloudflare/cloudflare-go/v5/zones"
 )
 
 type CloudflareDnsProvider struct {
 	client *cloudflare.Client
+	zone   *zones.Zone
 }
 
-func NewCloudflareDnsProvider(zoneID string) *CloudflareDnsProvider {
+func NewCloudflareDnsProvider(zoneName string) *CloudflareDnsProvider {
 	apiToken := os.Getenv("CLOUDFLARE_API_TOKEN")
-	client := cloudflare.NewClient(
-		option.WithAPIToken(apiToken), // defaults to os.LookupEnv("CLOUDFLARE_API_TOKEN")
-	)
+	if apiToken == "" {
+		os.Exit(1)
+	}
 
-	// zone, err := client.Zones.New(context.TODO(), zones.ZoneNewParams{
-	// 	Account: cloudflare.F(zones.ZoneNewParamsAccount{
-	// 		ID: cloudflare.F("023e105f4ecef8ad9ca31a8372d0c353"),
-	// 	}),
-	// 	Name: cloudflare.F("example.com"),
-	// 	Type: cloudflare.F(zones.TypeFull),
-	// })
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// fmt.Printf("%+v\n", zone.ID)
+	client := cloudflare.NewClient(
+		option.WithAPIToken(apiToken),
+	)
 
 	return &CloudflareDnsProvider{
 		client: client,
+		zone:   nil,
 	}
 }
 
-func (cdp *CloudflareDnsProvider) UpdateA(ip, nodeName string) error {
+func (cdp *CloudflareDnsProvider) UpdateA(ip, nodeName string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	zone, err := cdp.client.Zones.Get(ctx, zones.ZoneGetParams{})
-	if err != nil {
-		return fmt.Errorf("failed to get zone: %w", err)
+	// requestOptions = cloudflare.
+	recordParams := dns.ARecordParam{
+		Name:    cloudflare.F(nodeName),
+		TTL:     cloudflare.F(dns.TTL(60)),
+		Type:    cloudflare.F(dns.ARecordTypeA),
+		Content: cloudflare.F(ip),
+		Proxied: cloudflare.F(false),
 	}
-	record, err := cdp.client.DNS.Records.New(ctx, zone.ID, 
-		cdp.client.DNS.Options[zones.RecordNewParams{
-		Type:    "A",
-		Name:    nodeName,
-		Content: ip,
-		TTL:     60]
-	})
-	return nil
+
+	newParams := dns.RecordNewParams{
+		ZoneID: cloudflare.F(cdp.zone.ID),
+		Body:   recordParams,
+	}
+
+	options := option.WithMaxRetries(3)
+
+	_, err := cdp.client.DNS.Records.New(ctx, newParams, options)
+	if err != nil {
+		fmt.Printf("Failed to create A record: %v\n", err)
+	}
 }
